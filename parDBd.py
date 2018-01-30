df38560e9cf2ccc2a2831bbe8084446da296a094
joryleech <joryleech@gmail.com>
2018-01-29 21:51:03 -1000

    Added comments to runDDL Made Server Program Loop. Fixed SQL Statements

--- parDBd.py
+++ parDBd.py
@@ -15,22 +15,26 @@ print("Running Server Program")
 mySocket=socket.socket()
 
 mySocket.bind((IPAddress,int(Port)))
-print("Beginning Listening")
-mySocket.listen(1)
-conn,addr = mySocket.accept()
-print("Server Connection From "+ str(addr))
-data = conn.recv(4096).decode()
-print("Server Recieved:" + str(data))
-try: 
-	dbConnection.execute(str(data))
-	dbConnection.commit()
-	message = "SQL Command Succeeded"
-	conn.send(message.encode)
-except: 
-	print("Failed")
-	message = "SQL Command Failed"
-	conn.send(message.encode())
-
-conn.close()
+quit=0
+while quit == 0:
+	print("Beginning Listening")
+	mySocket.listen(1)
+	conn,addr = mySocket.accept()
+	print("Server Connection From "+ str(addr))
+	data = conn.recv(4096).decode()
+	print("Server Recieved:" + str(data))
+	if(str(data).rstrip=="quit"):
+		quit=1
+		break
+	try: 
+		dbConnection.execute(str(data))
+		dbConnection.commit()
+		message = "SQL Command Succeeded"
+		conn.send(message.encode())
+	except(sqlite3.Error) as e: 
+		print("Failed" + str(e))
+		message = "SQL Command Failed:\n"+str(e)
+		conn.send(message.encode())
+	conn.close()
 dbConnection.close()
 
--- runDDL.py
+++ runDDL.py
@@ -1,6 +1,11 @@
-import socket
-import sys
-import re
+import socket #Networking Library
+import sys #System Library. Use: Controlling Arguments
+import re #Regular Expression Library: Use parsing files.
+######
+#Class: Node
+#Function:Holds information about each node to encapsulate for single purpose.
+#
+######
 class Node:
 	nodeNumber=0
 	driver=""
@@ -9,8 +14,18 @@ class Node:
 		self.nodeNumber=number
 	def __str__(self):
 		return "Node#:"+self.nodeNumber+"\nDriver:"+self.driver+"\nhostname:"+self.hostname
+######
+#Name: Print Divide Line
+#Function: Prints a single line across the screen to improve readability
+#
+######
 def printDivideLine():
 	print("-------------------------------------------")
+######
+#Name: Set Nodes
+#Function: Takes information and sets node objects to the proper values
+#
+######
 def setNodes(listOfNodes, nodeID, infoType, value):
 	IDExists=0
 	for item in listOfNodes:
@@ -27,7 +42,9 @@ def setNodes(listOfNodes, nodeID, infoType, value):
 	if(infoType=="hostname"):
 		currentNode.hostname=value
 
-		
+#Manages Parameters for the program, 
+#Arg1 = clustercfg file
+#Arg2 = ddl file
 if(len(sys.argv)<3):
 	print("Please Specify ClusterCFG and DDLFile as Arg1 and Arg2 respectively")
 	exit()
@@ -38,6 +55,7 @@ print("ddlfile="+ddlfile)
 print("Reading ClusterCFG File")
 clustercfg = open(clustercfg,"r")
 listOfNodes=[]
+#Parses the clustercfg file for pertinant information about regular nodes.
 for line in clustercfg:
 	if(line!="\n"):
 		temp = line.split("=")
@@ -45,9 +63,10 @@ for line in clustercfg:
 		tempnode=re.findall(r'\d+', temp[0].split(".")[0])[0]
 		tempinfo=temp[0].split(".")[1].rstrip()
 		setNodes(listOfNodes,tempnode,tempinfo,tempvalue)
-
 clustercfg.close()
 printDivideLine()
+
+#Reads DDL into memory so that it may be sent later. Uses concatenation to avoid readline constraints.
 for item in listOfNodes:
 	print(item)
 printDivideLine()
@@ -58,12 +77,19 @@ for line in ddlfile:
 	ddlContents+=line;
 print(ddlContents)
 printDivideLine()
+
 mySocket= socket.socket()
 print("Beginning Connection to Nodes")
 for node in listOfNodes:
-	print("Connecting to Node#:"+node.nodeNumber)
-	splitHost = node.hostname.split(":")
-	print("IPAddress:"+splitHost[0]+" || Port:"+ splitHost[1])
-	mySocket.connect((splitHost[0],int(splitHost[1].split("/")[0])))
-
+	try:
+		print("Connecting to Node#:"+node.nodeNumber)
+		splitHost = node.hostname.split(":")
+		print("IPAddress:"+splitHost[0]+" || Port:"+ splitHost[1])
+		mySocket.connect((splitHost[0],int(splitHost[1].split("/")[0])))
+		mySocket.send(ddlContents.encode())
+		data = mySocket.recv(4096).decode()
+		print("Node["+node.nodeNumber+"] Results:"+data)
+		mySocket.close()
+	except:
+		print("Failed, unable to connect to node#"+node.nodeNumber)
 
